499b47b36414c865e7b20f5e4f054a0c321ea18e
Henrique Sobral Gomes <henrique@devideia.com.br>
2016-06-22 09:41:09 -0300

    Initial Commit

---
+++ CieloIOS/CieloIOS.h
@@ -0,0 +1,19 @@
+//
+//  CieloIOS.h
+//  CieloIOS
+//
+//  Created by Henrique Sobral on 22/06/16.
+//  Copyright © 2016 Sallute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CieloIOS.
+FOUNDATION_EXPORT double CieloIOSVersionNumber;
+
+//! Project version string for CieloIOS.
+FOUNDATION_EXPORT const unsigned char CieloIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CieloIOS/PublicHeader.h>
+
+
